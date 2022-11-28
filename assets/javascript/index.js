b0138d62fd53c6021829fc2470b8a34720bc8162
Felix ADEWALE <112468468+afunmibi@users.noreply.github.com>
2022-11-28 09:57:07 +0000

    Add files via upload

---
+++ assets/javascript/index.js
@@ -0,0 +1,21 @@
+
+ 
+  let x = document.forms["contactform"]["fname"].value;
+  let y = document.forms["contactform"]["email"].value;
+
+function showup(){
+    
+    if(x=== ""){
+ alert("Input must be filled");
+    }
+  
+    if(y=== ""){
+     alert("Input must be filled");
+ }else{
+
+         alert("Thank you for your message");
+     }
+
+ }
+
+
